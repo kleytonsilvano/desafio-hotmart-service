c307a7a2ff807d1792396e483ecbb3210f1a0627
kleytonsilvano <kleyton.silvano@gmail.com>
2020-11-14 22:35:16 -0300

    adding custom variables on application properties - @value on controller getting the variable

--- src/main/java/com/hotmart/controllers/ProductController.java
+++ src/main/java/com/hotmart/controllers/ProductController.java
@@ -8,21 +8,21 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hotmart.converter.ProductModelConverter;
 import com.hotmart.exceptions.BadRequestException;
 import com.hotmart.models.Product;
 import com.hotmart.models.ProductCategory;
 import com.hotmart.repository.ProductCategoryRepository;
 import com.hotmart.repository.ProductRepository;
-import com.hotmart.utils.ClassConverter;
 
 import gen.api.ProductsApi;
-import gen.models.ProductCreationModel;
 import gen.models.ProductModel;
 
 @RestController
@@ -33,14 +33,19 @@ public class ProductController implements ProductsApi {
 
 	@Autowired
 	private ProductCategoryRepository productCategoryRepository;
+	
+	@Autowired
+	private ProductModelConverter converter;
+	
+	@Value("${DEFAULT_PAGE_SIZE}")
+	private Integer defaultPageSize;
 
 	@Override
-	public ResponseEntity<Void> addProduct(@Valid @RequestBody ProductCreationModel body) {
+	public ResponseEntity<Void> addProduct(@Valid @RequestBody ProductModel body) {
 		
     	if( body != null ) {
 
-    		List<ProductCategory> categories = findCategories(body);
-    		saveProduct(body, categories);
+    		saveProduct(body);
     		
     		return new ResponseEntity<>(HttpStatus.CREATED);
     		
@@ -50,47 +55,14 @@ public class ProductController implements ProductsApi {
 
     }
 
-	private List<ProductCategory> findCategories(ProductCreationModel body) {
-		
-		List<ProductCategory> listReturn = new ArrayList<>();
-		
-		for (String category : body.getCategories()) {
-			
-			ProductCategory prodCategory = productCategoryRepository.findByName(category);
-			
-			if(prodCategory != null) {
-				
-				throw new BadRequestException();
-				
-			} else {
-				
-				listReturn.add(prodCategory);
-				
-			}
-			
-		}
-		
-		return listReturn;
-		
-	}
-
-	private void saveProduct(ProductCreationModel body, List<ProductCategory> categories) {
-		Product product = (Product) ClassConverter.copyProperties(new Product(), body.getProduct());
-		product.setCreationDate(new Date());
-		product.setCategories(categories);
-		repository.save(product);
-	}
-
-	@SuppressWarnings("unchecked")
 	@Override
     public ResponseEntity<List<ProductModel>> listProducts() {
-    	
+		
     	List<Product> products = repository.findAll();
     	
     	if( products != null && !products.isEmpty()) {
     		
-    		List<ProductModel> r = (List<ProductModel>) ClassConverter.copyProperties(new ArrayList<ProductModel>(), products);
-    		return new ResponseEntity<>(r, HttpStatus.OK);
+    		return new ResponseEntity<>(converter.converter(products), HttpStatus.OK);
     		
     	}
     	
@@ -101,16 +73,51 @@ public class ProductController implements ProductsApi {
 	@Override
 	public ResponseEntity<ProductModel> findProductById(@PathVariable("idProduct") Long idProduct) {
 		
-		Optional<Product> products = repository.findById(idProduct);
+		Optional<Product> product = repository.findById(idProduct);
     	
-    	if(products.isPresent()) {
+    	if(product.isPresent()) {
     		
-    		ProductModel r = (ProductModel) ClassConverter.copyProperties(new ProductModel(), products);
-    		return new ResponseEntity<>(r, HttpStatus.OK);
+    		return new ResponseEntity<>(converter.converter(product.get()), HttpStatus.OK);
     		
     	}
 		
 		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		
+	}
+	
+	
+	private List<ProductCategory> findCategories(ProductModel body) {
+		
+		List<ProductCategory> listReturn = new ArrayList<>();
+		
+		for (String category : body.getCategories()) {
+			
+			ProductCategory prodCategory = productCategoryRepository.findByName(category);
+			
+			if(prodCategory != null) {
+				
+				listReturn.add(prodCategory);
+				
+			} else {
+				
+				throw new BadRequestException();
+				
+			}
+			
+		}
+		
+		return listReturn;
+		
+	}
+
+	private void saveProduct(ProductModel body) {
+		
+		Product product = new Product();
+		product.setCreationDate(new Date());
+		product.setCategories(findCategories(body));
+		product.setDescription(body.getDescription());
+		product.setName(body.getName());
+		repository.save(product);
 		
 	}
 }
